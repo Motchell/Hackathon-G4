a4ede923292d6572a9faf1ae681541ede8b1ffe0
Motchell <hidalgomora.ma@gmail.com>
2024-07-05 12:42:50 -0400

    updating CalendarService

--- src/main/java/com/forge/hackathon/services/CalendarService.java
+++ src/main/java/com/forge/hackathon/services/CalendarService.java
@@ -1,5 +1,20 @@
 package com.forge.hackathon.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.forge.hackathon.repositories.CalendarRepository;
+
+import java.util.Optional;
+
+@Service
 public class CalendarService {
-
+	@Autowired
+	private static CalendarRepository calendarRepository;
+	
+	public Calendar obtenerUnoPorId(Long id) {
+		Optional<Calendar> calendarFounded = calendarRepository.findById(id);
+		return calendarFounded.get();
+	}
 }
+	
+	
