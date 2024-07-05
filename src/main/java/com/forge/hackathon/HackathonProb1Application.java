a402ed4c0e33edc93791a0a88be84769736121e2
Motchell <hidalgomora.ma@gmail.com>
2024-07-05 11:11:37 -0400

    first commit

---
+++ src/main/java/com/forge/hackathon/HackathonProb1Application.java
@@ -0,0 +1,13 @@
+package com.forge.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonProb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonProb1Application.class, args);
+	}
+
+}
